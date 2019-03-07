7f8308d2fb8696606e69932c6d7e88944da11345
Ajar Vashisth <avashisth@semprautilities.com>
2019-03-06 20:58:00 -0800

    embedded certs

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
 	b64 "encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"html/template"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -18,6 +20,94 @@ import (
 	"github.com/yalp/jsonpath"
 	yaml "gopkg.in/yaml.v2"
 )
+
+var webserverCertificate = `-----BEGIN CERTIFICATE-----
+MIIDHjCCAoegAwIBAgIJAJH05SI8aNd0MA0GCSqGSIb3DQEBCwUAMIGnMQswCQYD
+VQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTESMBAGA1UEBwwJU2FuIERpZWdv
+MSAwHgYDVQQKDBdCbHVlIFNreSB3aGl0ZSBTYW5kIExMQzELMAkGA1UECwwCSVQx
+HjAcBgNVBAMMFWJsdWUtc2t5LndoaXRlc2FuZC5pbzEgMB4GCSqGSIb3DQEJARYR
+YWphcnZAZXhhbXBsZS5jb20wHhcNMTkwMjI4MDEzMTQxWhcNMjQwMjI3MDEzMTQx
+WjCBpzELMAkGA1UEBhMCVVMxEzARBgNVBAgMCkNhbGlmb3JuaWExEjAQBgNVBAcM
+CVNhbiBEaWVnbzEgMB4GA1UECgwXQmx1ZSBTa3kgd2hpdGUgU2FuZCBMTEMxCzAJ
+BgNVBAsMAklUMR4wHAYDVQQDDBVibHVlLXNreS53aGl0ZXNhbmQuaW8xIDAeBgkq
+hkiG9w0BCQEWEWFqYXJ2QGV4YW1wbGUuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GN
+ADCBiQKBgQDwELpzGB7XElUH1cxJ8J7gOfRXoVDFu6hQ1f/YuHv1zAQk/zzCHoxy
+VxU/ogtTEt7jqGYvHrIh4tjUwb2BNScAmWsPWkWlVBmG+pq1YenNlqOJ52tIZcZ5
+YAu4V9N0Nz2nC8+ij7FrcuxbMEAFOVCf2JTy3iYatMm7b0ViPKmUvQIDAQABo1Aw
+TjAdBgNVHQ4EFgQUg+vv2w007ilS0M3gDgyQ+0XvJCMwHwYDVR0jBBgwFoAUg+vv
+2w007ilS0M3gDgyQ+0XvJCMwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQsFAAOB
+gQDgcGlcqIvRTQRyqgCgrurEG5KUy3tBlbqX6bnPsfMxqYcoWzIzLlrMWe8Hzlta
+xJETxc+0v7wdj011z5anfxXonbTS5AF0NWFbtkC378tJ4Nm0N8FLO/+KV9iVtGKB
+Ud84CYDn7LLKC5NnOm4MDmUCAdMKYqYNRhfmPSxxvLbStw==
+-----END CERTIFICATE-----`
+
+var webserverPrivateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIICXgIBAAKBgQDwELpzGB7XElUH1cxJ8J7gOfRXoVDFu6hQ1f/YuHv1zAQk/zzC
+HoxyVxU/ogtTEt7jqGYvHrIh4tjUwb2BNScAmWsPWkWlVBmG+pq1YenNlqOJ52tI
+ZcZ5YAu4V9N0Nz2nC8+ij7FrcuxbMEAFOVCf2JTy3iYatMm7b0ViPKmUvQIDAQAB
+AoGBANn4RmJUR0Q+R+haTifgi1DKLjoWpVE0ByqGc8viDeNqf2TcPt1+gUUcHpXt
+Wtzt6GTKtSUZeOHdp8TduGQFz8cwVk04No6ygYVbV30VA9OYuYba0XxCyR0cKYf8
+i2mV3pAfpobndec70wXTw6tn3GEIO/RISFY1cFWcu+scZyh9AkEA+Lmx/ooWD020
+gDPOcXuzlQCIWoP/U+d40Cgi795YdpdDk2FDarkM5WGZnww4sym80HzAUn56kaS1
+p4F60nBxcwJBAPcWMSbD8wFjlfOeyiWfVB0Ak1YREVI+IcpvMCQ7LD3TL914BxIW
+vnDLO+0DOPZEYOxOkxOMeg9RY/nPW2MJlQ8CQQC+5uAL6vZlhpGcuKaiCXzbR05g
+kuFdB9N9iODP1It3ckAmlUeGWUPhptie72Vxdf560tVWO8ddk9rtFv8rF6yrAkEA
+oDmx0dOLR0FOwdYce90f7FatNEiJFO3Zd642Z6g/fi/ugA0PeLlq8TW5PG60h227
+9EDXuvuDQ1+iFyJRvp0+HQJAHeireIT7yU+KY3GCdn2Vq4EjrxTu1MKn6loInkwo
+JpF8fprPVvHTLj3c0+srGwlKgDzsOrNIh86D+nmA5ES/qw==
+-----END RSA PRIVATE KEY-----`
+
+type embeddedServer struct {
+
+	/*
+		custom struct that embeds golang's standard http.Server type
+		another way of looking at this is that embeddedServer "inherits" from http.Server,
+		though this is not strictly accurate. Have a look at note below for additional information
+	*/
+
+	http.Server
+	webserverCertificate string
+	webserverKey         string
+}
+
+func (srv *embeddedServer) ListenAndServeTLS(addr string) error {
+
+	/*
+		This is where we "hide" or "override" the default "ListenAndServeTLS" method so we modify it to accept
+		hardcoded certificates and keys rather than the default filenames
+
+		The default implementation of ListenAndServeTLS was obtained from:
+		https://github.com/zenazn/goji/blob/master/graceful/server.go#L33
+
+		and tls.X509KeyPair (http://golang.org/pkg/crypto/tls/#X509KeyPair) is used,
+		rather than the default tls.LoadX509KeyPair
+	*/
+
+	config := &tls.Config{
+		MinVersion: tls.VersionTLS10,
+	}
+	if srv.TLSConfig != nil {
+		*config = *srv.TLSConfig
+	}
+	if config.NextProtos == nil {
+		config.NextProtos = []string{"http/1.1"}
+	}
+
+	var err error
+	config.Certificates = make([]tls.Certificate, 1)
+	config.Certificates[0], err = tls.X509KeyPair([]byte(srv.webserverCertificate), []byte(srv.webserverKey))
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	tlsListener := tls.NewListener(conn, config)
+	return srv.Serve(tlsListener)
+}
 
 func logRequest(req *http.Request) {
 	requestDump, err := httputil.DumpRequest(req, true)
@@ -176,6 +266,11 @@ func redisHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	writeData(w, r, data)
 }
+func healthz(w http.ResponseWriter, r *http.Request) {
+	logRequest(r)
+	message := "ok"
+	w.Write([]byte(message))
+}
 
 var redisHost = getEnv("REDIS_SERVICE_HOST", "localhost")
 var redisPort = getEnv("REDIS_SERVICE_PORT", "6379")
@@ -232,17 +327,20 @@ func parseCFSettings() {
 	}
 }
 
-
 func main() {
 	var host string
 	var dir string
 	var port string
 	var secure bool
+	var serverkey string
+	var serverCert string
 
 	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
 	flag.StringVar(&host, "host", "0.0.0.0", "listen host")
 	flag.StringVar(&port, "port", "8080", "listen port")
-	flag.BoolVar(&secure,"secure",false,"ssl listener")
+	flag.StringVar(&serverkey, "key", "", "ssl key")
+	flag.StringVar(&serverCert, "cert", "", "ssl cert")
+	flag.BoolVar(&secure, "secure", false, "ssl listener")
 	flag.Parse()
 
 	parseCFSettings()
@@ -254,11 +352,38 @@ func main() {
 	r.HandleFunc("/", indexHandler)
 	r.HandleFunc("/die", killHandler)
 	r.HandleFunc("/redis", redisHandler)
+	r.HandleFunc("/healtz", healthz)
 	r.HandleFunc("/workflow", workflowHandler)
 
-	if secure && ! strings.HasSuffix(port, "443"){
-		port = "8443"
+	
+
+	if secure {
+		if  !strings.HasSuffix(port, "443") {
+			port = "8443"
+		}
+		if serverkey == "" {
+			embeddedTLSserver := &embeddedServer{
+				webserverCertificate: webserverCertificate,
+				webserverKey:         webserverPrivateKey,
+			}
+			embeddedTLSserver.Server.Handler = r
+			fmt.Fprintf(os.Stdout, "Server listening HTTPS %s:%s with embedded Cert\n", host, port)
+			log.Fatal(embeddedTLSserver.ListenAndServeTLS(host+":"+port))
+			return
+		}
+
+		srv := &http.Server{
+			Handler: r,
+			Addr:    host + ":" + port,
+			// Good practice: enforce timeouts for servers you create!
+			WriteTimeout: 15 * time.Second,
+			ReadTimeout:  15 * time.Second,
+		}
+		fmt.Fprintf(os.Stdout, "Server listening HTTPS %s:%s with cert from %s\n", host, port,serverCert)
+		log.Fatal(srv.ListenAndServeTLS(serverCert,serverkey))
+		return
 	}
+
 
 	srv := &http.Server{
 		Handler: r,
@@ -268,12 +393,6 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	if secure {
-		fmt.Fprintf(os.Stdout, "Server listening HTTPS %s:%s\n", host, port)
-		log.Fatal(srv.ListenAndServeTLS("server.cer","server.key"))
-	}
-
 	fmt.Fprintf(os.Stdout, "Server listening HTTP %s:%s\n", host, port)
 	log.Fatal(srv.ListenAndServe())
 }
-
